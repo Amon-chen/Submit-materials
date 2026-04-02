620723edb4e0e133f423da7310901a0eb71c8fb8
Amon-chen <cly1456480869@gmail.com>
2026-04-02 11:50:41 +0900

    2026.4.2　第一回目課題提出

---
+++ Main.java
@@ -0,0 +1,13 @@
+package variabale;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int age;
+	age = 25;
+	String name ="CHEN LUYI";
+	System.out.println("私は" + name + "です。年齢は" + age + "歳です。");
+	
+	}
+
+}
